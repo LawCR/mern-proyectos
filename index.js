b19acb9a375edaa16d7a4fd59ed77ee2ef3374d2
LawCR <alvacalderon99@gmail.com>
2021-12-06 02:03:08 -0500

    ajustes para deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use( express.json ({extended: true}))
 
 // Puerto de la app
-const PORT = process.env.PORT || 8081
+const port = process.env.port || 8081
 
 // Importar Rutas
 app.use('/api/usuarios', require('./routes/usuarios'))
@@ -23,6 +23,6 @@ app.use('/api/proyectos', require('./routes/proyectos'))
 app.use('/api/tareas', require('./routes/tareas'))
 
 // Arrancar la app
-app.listen(PORT, () => {
-    console.log(`El servidor esta funcionando en el puerto ${PORT}`);
+app.listen(port, '0.0.0.0', () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`);
 })
